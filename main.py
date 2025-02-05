b31458506c6f4eaf4e254880c123aa5396c5ffcd
Will <115461530+Will-Cross1@users.noreply.github.com>
2025-02-05 08:54:28 +0000

    Added more special characters to password checking
    Co-authored-by: rowan04 <rowan.moss@stfc.ac.uk>

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ class SmartHomeApp:
         has_lower = any(c.islower() for c in password)
         has_upper = any(c.isupper() for c in password)
         has_digit = any(c.isdigit() for c in password)
-        has_special = any(c in "!@#$%^&*()_+" for c in password)
+        has_special = any(c in "!?@#$%^&*(){}[]_-+=£/\\|.,~¬'\"" for c in password)
 
         if not (has_lower and has_upper and has_digit and has_special):
             log.info(
